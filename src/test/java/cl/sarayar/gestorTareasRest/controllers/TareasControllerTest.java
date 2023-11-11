eb62014d06cc6ec81ccbe56a28245af153957fe3
Cristobal-coding <agartkg95@gmail.com>
2023-11-11 19:19:50 -0300

    Finish Test for Controllers part 2

--- src/test/java/cl/sarayar/gestorTareasRest/controllers/TareasControllerTest.java
+++ src/test/java/cl/sarayar/gestorTareasRest/controllers/TareasControllerTest.java
@@ -1,4 +1,113 @@
+package cl.sarayar.gestorTareasRest.controllers;
+
+import cl.sarayar.gestorTareasRest.entities.Tarea;
+import cl.sarayar.gestorTareasRest.services.TareasService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
 class TareasControllerTest {
-  
+    @Mock
+    private TareasService tareasService;
+    private  TareasController tareasController;
+    private Tarea tarea;
+
+    @BeforeEach
+    void init(){
+        MockitoAnnotations.initMocks(this);
+        this.tareasController = new TareasController(tareasService);
+        tarea = new Tarea();
+        tarea.setId("1");
+        tarea.setDescripcion("Descripción actualizada");
+        tarea.setVigente(true);
+    };
+
+    @Test
+    void getAllOk()  {
+        List<Tarea> tareasSimuladas = new ArrayList<>();
+        Tarea tarea2 = new Tarea();
+        tarea2.setId("2");
+        tarea2.setIdentificador(2L);
+        tarea2.setDescripcion("Tarea 2");
+        tarea2.setFechaCreacion(LocalDateTime.now());
+        tarea2.setVigente(false);
+
+        tareasSimuladas.add(tarea);
+        tareasSimuladas.add(tarea2);
+
+        when(tareasService.findAll()).thenReturn(tareasSimuladas);
+
+        List<Tarea> resultado = tareasController.getAll();
+
+        assertEquals(tareasSimuladas.size(), resultado.size());
+        assertEquals(tareasSimuladas, resultado);
+    }
+    @Test
+    void saveOk()  {
+        when(tareasService.save(any(Tarea.class))).thenReturn(tarea);
+        ResponseEntity<Tarea> result = tareasController.save(tarea);
+
+        assertEquals(tarea, result.getBody());
+        assertEquals(HttpStatus.OK, result.getStatusCode());
+    }
+    @Test
+    void updateOk()  {
+        Tarea tareaOriginal = new Tarea();
+        tareaOriginal.setId("1");
+        tareaOriginal.setDescripcion("Descripción original");
+        tareaOriginal.setVigente(true);
+        tareaOriginal.setFechaCreacion(LocalDateTime.now());
+
+        when(tareasService.findById(tarea.getId())).thenReturn(tareaOriginal);
+        when(tareasService.save(any(Tarea.class))).thenReturn(tarea);
+
+        ResponseEntity<Tarea> result = tareasController.update(tarea);
+
+        assertEquals(tarea, result.getBody());
+    }
+
+    @Test
+    void updateOkTestException(){
+
+        String taskId = "2";
+        when(tareasService.findById(taskId)).thenReturn(null);
+
+        ResponseEntity<Tarea> result = tareasController.update(new Tarea());
+
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, result.getStatusCode());
+        assertNull(result.getBody());
+    }
+    @Test
+    void deleteOk()  {
+        String id = "1";
+        when(tareasService.remove(id)).thenReturn(true);
+        ResponseEntity<Boolean> result = tareasController.delete(id);
+
+        assertEquals(HttpStatus.OK, result.getStatusCode());
+    }
+
+    @Test
+    void deleteOkTestException(){
+
+        String idException = "33";
+        when(tareasService.remove(idException)).thenThrow(new RuntimeException());
+
+        ResponseEntity<Boolean> result = tareasController.delete(idException);
+
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, result.getStatusCode());
+        assertFalse(result.getBody());
+    }
+
+
 }
--- src/test/java/cl/sarayar/gestorTareasRest/controllers/UsuariosControllerTest.java
+++ src/test/java/cl/sarayar/gestorTareasRest/controllers/UsuariosControllerTest.java
@@ -1,4 +1,127 @@
+package cl.sarayar.gestorTareasRest.controllers;
+
+
+import cl.sarayar.gestorTareasRest.config.auth.dto.MessageResponse;
+import cl.sarayar.gestorTareasRest.entities.Usuario;
+import cl.sarayar.gestorTareasRest.services.UsuariosService;
+import cl.sarayar.gestorTareasRest.utils.JwtUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
 class UsuariosControllerTest {
-  
+    @Mock
+    private UsuariosService usService;
+    @Mock
+    private JwtUtils jwtUtils;
+
+    private  UsuariosController usuariosController;
+    private Usuario usuario;
+
+    @BeforeEach
+    void init(){
+        MockitoAnnotations.initMocks(this);
+        this.usuariosController = new UsuariosController(usService, jwtUtils);
+
+        usuario = new Usuario();
+        usuario.setId("1");
+        usuario.setNombre("Criss");
+        usuario.setCorreo("Criss@usm.cl");
+        usuario.setClave("123456");
+    };
+
+    @Test
+    void loginOk(){
+
+
+        ResponseEntity<?> result = usuariosController.authenticateUser(usuario);
+        assertEquals(200, result.getStatusCodeValue());
+        assertEquals(usuario, result.getBody());
+    }
+
+    @Test
+    void registerUserOk() {
+
+        when(usService.existsByCorreo("Criss@usm.cl")).thenReturn(false);
+        when(usService.save(any())).thenReturn(usuario);
+        ResponseEntity<?> result = usuariosController.registerUser(usuario);
+
+        assertEquals(HttpStatus.OK, result.getStatusCode());
+
+    }
+
+    @Test
+    void registerUserOkErrorPorCorreoExistente() {
+
+        when(usService.existsByCorreo("Criss@usm.cl")).thenReturn(true);
+        when(usService.save(any())).thenReturn(usuario);
+        ResponseEntity<?> result = usuariosController.registerUser(usuario);
+
+        assertEquals(HttpStatus.BAD_REQUEST, result.getStatusCode());
+        assertEquals("Error: Usuario ya existe!", ((MessageResponse) result.getBody()).getMensaje());
+
+    }
+
+    @Test
+    void actualizarOk() {
+        Usuario usuarioOriginal = new Usuario();
+        usuarioOriginal.setId("1");
+        usuarioOriginal.setNombre("CrissOld");
+        usuarioOriginal.setCorreo("CrissOld@usm.cl");
+        usuarioOriginal.setClave("98765");
+
+        when(usService.findById(usuario.getId())).thenReturn(usuarioOriginal);
+        //El correo nuevo a actualizar, no existirá previamente( Criss@usm.cl ).
+        when(usService.findByCorreo(usuario.getCorreo())).thenReturn(null);
+        when(usService.save(any())).thenReturn(usuario);
+
+        ResponseEntity<?> result = usuariosController.actualizarUsuario(usuario);
+
+        assertEquals(HttpStatus.OK, result.getStatusCode());
+    }
+
+    @Test
+    void actualizarUsuarioErrorCorreoExistente() {
+        Usuario usuarioOriginal = new Usuario();
+        usuarioOriginal.setId("1");
+        usuarioOriginal.setNombre("CrissOld");
+        usuarioOriginal.setCorreo("CrissOld@usm.cl");
+        usuarioOriginal.setClave("98765");
+
+        Usuario usuarioConCorreo = new Usuario();
+        usuarioConCorreo.setId("3");
+
+        when(usService.findById(usuario.getId())).thenReturn(usuarioOriginal);
+        when(usService.findByCorreo(usuario.getCorreo())).thenReturn(usuarioConCorreo);
+
+        ResponseEntity<?> result = usuariosController.actualizarUsuario(usuario);
+
+        assertEquals(HttpStatus.BAD_REQUEST, result.getStatusCode());
+        assertEquals("Error: Correo se encuentra utilizado!", ((MessageResponse) result.getBody()).getMensaje());
+    }
+
+    @Test
+    void getAllUsuarios() {
+        List<Usuario> usuariosSimulados = new ArrayList<>();
+        usuariosSimulados.add(new Usuario("10", "Usuario1", "usuario1@usm.com", "123", 1));
+        usuariosSimulados.add(new Usuario("20", "Usuario2", "usuario2@usm.com", "456", 1));
+        usuariosSimulados.add(new Usuario("39", "Usuario3", "usuario3@usm.com", "789", 1));
+
+
+        when(usService.getAll()).thenReturn(usuariosSimulados);
+        List<Usuario> resultado = usuariosController.getAll();
+
+        assertEquals(usuariosSimulados.size(), resultado.size());
+        assertEquals(usuariosSimulados, resultado);
+    }
 }
