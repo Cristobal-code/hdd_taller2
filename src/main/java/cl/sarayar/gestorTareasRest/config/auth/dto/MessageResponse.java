a7ec02b69f1ea25c4af400db9e004ee841d8db0c
Cristobal-coding <agartkg95@gmail.com>
2023-11-11 16:50:12 -0300

    Fixing import lombok to @generated s

--- src/main/java/cl/sarayar/gestorTareasRest/config/auth/dto/MessageResponse.java
+++ src/main/java/cl/sarayar/gestorTareasRest/config/auth/dto/MessageResponse.java
@@ -1,9 +1,6 @@
 package cl.sarayar.gestorTareasRest.config.auth.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 @Getter
 @Setter
